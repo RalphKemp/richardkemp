2f66a408c76d8b6810fdf8a46d8184df57fbd19f
ralph kemp <ralphk3mp@gmail.com>
2017-09-15 18:36:09 +0100

    fixed nav links, pages working

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations"}
   root to: 'pages#home'
   resources :albums
+  get 'about', to: 'pages#about'
+  get 'blog', to: 'pages#blog'
 end
