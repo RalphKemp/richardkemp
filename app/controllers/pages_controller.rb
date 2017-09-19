bb078e0aa14a9111c8fdbf13edd45c7b58767025
ralph kemp <ralphk3mp@gmail.com>
2017-09-19 21:40:10 +0100

    got photos showing on home page but still just text, not actual photos

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :about, :blog]
 
   def home
+    @album = Album.all.sample
   end
 
   def about
@@ -11,3 +12,6 @@ class PagesController < ApplicationController
   end
 
 end
+
+
+
