7bd5f448ad821657a3cbc84ffdf12300f01a6e55
ralph kemp <ralphk3mp@gmail.com>
2017-09-27 00:43:43 +0100

    changed album schema description to text

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170917194651) do
+ActiveRecord::Schema.define(version: 20170926234121) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20170917194651) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
-    t.string "description"
+    t.text "description"
   end
 
   create_table "attachinary_files", id: :serial, force: :cascade do |t|
