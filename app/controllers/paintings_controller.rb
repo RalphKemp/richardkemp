f07d963722410f6d2beb33a77f9da4e1a25ddd38
ralph kemp <ralphk3mp@gmail.com>
2017-10-20 01:36:15 +0100

    photo uploads working

--- app/controllers/paintings_controller.rb
+++ app/controllers/paintings_controller.rb
@@ -1,50 +1,54 @@
 class PaintingsController < ApplicationController
-
-  def index
-    @paintings = Painting.all
-  end
+  before_action :set_album
 
   def new
-    @painting = Painting.new
+    @painting = @album.paintings.new
+  end
+
+   def show
+    @painting = @album.paintings.find(params[:id])
   end
 
   def create
-    @album = Album.find(params[:id])
-    @painting = Painting.new(painting_params)
+    @painting = @album.paintings.new(painting_params)
     @painting.album = @album
     if @painting.save
       flash[:notice] = "Successfully created painting."
-      redirect_to album_path(@album)
+      redirect_to @album
     else
-      render 'new'
+      render :new
     end
   end
 
   def edit
-    @painting = Painting.find(params[:id])
+    @painting = @album.paintings.find(params[:id])
   end
 
   def update
-    @painting = Painting.find(params[:id])
-    if @painting.update_attributes(painting_params)
+    @painting = @album.paintings.find(params[:id])
+    if @painting.update(painting_params)
       flash[:notice] = "Successfully updated painting."
-      redirect_to @painting.album
+      redirect_to @album
     else
       render 'new'
     end
   end
 
   def destroy
-    @painting = Painting.find(params[:id])
+    @painting = @album.paintings.find(params[:id])
     @painting.destroy
     flash[:notice] = "Successfully destroyed painting."
-    redirect_to @painting.gallery
+    redirect_to @album
   end
 
   private
 
   def painting_params
     params.require(:painting).permit(:name, :description, :album_id, :photo, :photo_cache)
+  end
+
+  def set_album
+    @album = Album.find(params[:album_id])
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: "registrations"}
   root to: 'pages#home'
-  resources :albums
+  resources :albums do
     resources :paintings
-
+  end
   resources :posts
   get 'about', to: 'pages#about'
 end
