01a8b4177de3bb56137821bf7be4dd89ece0e87a
ralph kemp <ralphk3mp@gmail.com>
2017-10-23 01:46:09 +0100

    meh

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,6 +2,7 @@
 //= require jquery_ujs
 //= require bootstrap-sprockets
 //= require jquery-fileupload/basic
+//= require bootsy
 //= require cloudinary/jquery.cloudinary
 //= require lightbox
 //= require_tree .
